e684f7ac899c2bc9c0a546838a4bbd324ce67e0d
Simon The Sourcerer <ssulser@gmail.com>
2026-08-08 22:50:21 +0200

    Updated GnuCOBOL formula

--- Formula/gnucobol-svn.rb
+++ Formula/gnucobol-svn.rb
@@ -9,6 +9,7 @@ class GnucobolSvn < Formula
   depends_on "automake" => :build
   depends_on "bison" => :build
   depends_on "flex" => :build
+  depends_on "help2man" => :build
   depends_on "libtool" => :build
   depends_on "pkgconf" => :build
   depends_on "subversion" => :build
@@ -22,17 +23,45 @@ class GnucobolSvn < Formula
   depends_on "ssulser/local/vbisam"
 
   def install
-    # Ensure bootstrap and build use the same Homebrew GNU libtool.
-    ENV.prepend_path "PATH", Formula["libtool"].opt_bin
-    ENV["LIBTOOL"] = Formula["libtool"].opt_bin/"glibtool"
-    ENV["LIBTOOLIZE"] = Formula["libtool"].opt_bin/"glibtoolize"
+    libtool = Formula["libtool"]
+    gettext = Formula["gettext"]
+    ncurses = Formula["ncurses"]
 
-    # Generate configure and the remaining Autotools infrastructure.
-    system "build_aux/bootstrap"
+    # Force a consistent Homebrew GNU libtool toolchain.
+    ENV.prepend_path "PATH", libtool.opt_bin
+    ENV["LIBTOOL"] = libtool.opt_bin/"glibtool"
+    ENV["LIBTOOLIZE"] = libtool.opt_bin/"glibtoolize"
 
-    # Use Homebrew ncurses and explicitly link the panel library.
-    ENV.append "CPPFLAGS", "-I#{Formula["ncurses"].opt_include}"
-    ENV.append "LDFLAGS", "-L#{Formula["ncurses"].opt_lib}"
+    # Make Homebrew gettext tools available during regeneration
+    # of the autotools/gettext infrastructure.
+    ENV.prepend_path "PATH", gettext.opt_bin
+
+    # Recreate libtool files using the same libtool version that
+    # will later be used by the build.
+    system libtool.opt_bin/"glibtoolize",
+           "--force",
+           "--copy"
+
+    # Recreate gettext infrastructure so po/Makefile.in.in and
+    # the gettext autoconf macros use the same gettext version.
+    system gettext.opt_bin/"gettextize",
+           "--force",
+           "--copy",
+           "--no-changelog"
+
+    # Generate configure and the remaining autotools files.
+    system "autoreconf", "-fi"
+
+    # Current GnuCOBOL SVN sources do not compile cleanly as C23.
+    # C17 retains compatibility with the older unspecified-argument
+    # function declarations still present in the source tree.
+    ENV["CFLAGS"] = "-O2 -std=gnu17"
+
+    # Use Homebrew ncurses explicitly.
+    ENV.append "CPPFLAGS", "-I#{ncurses.opt_include}"
+    ENV.append "LDFLAGS", "-L#{ncurses.opt_lib}"
+
+    # GnuCOBOL screen I/O uses functions from the ncurses panel library.
     ENV.append "LIBS", "-lpanel"
 
     system "./configure",
